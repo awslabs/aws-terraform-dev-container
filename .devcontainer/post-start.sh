90e4a99b8a88eb727597f01dc636d9906b82dd4f
Valter Silva <valterh@amazon.com>
2022-10-27 15:26:37 +0000

    Display welcome message post-start

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -3,5 +3,4 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-# Install AWS Code Habits | https://github.com/awslabs/aws-code-habits
-curl -sL https://raw.githubusercontent.com/awslabs/aws-code-habits/main/scripts/remote/init.sh | bash
+clear && printf "\e[0;32mAWS Terraform Dev Container: $(basename $PWD)\e[0m\n"
