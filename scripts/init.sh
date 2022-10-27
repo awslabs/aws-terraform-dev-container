2eead60b7b5f7c5ae4500ecb1b93b45f3918d2cc
Valter Silva <valterh@amazon.com>
2022-10-27 15:26:50 +0000

    Install AWS Code Habits during installation

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,3 +7,6 @@ set -o pipefail
 git clone --branch=main --depth=1 git@github.com:awslabs/aws-terraform-dev-container.git
 mv aws-terraform-dev-container/.devcontainer .devcontainer
 rm -rf aws-terraform-dev-container
+
+# Install AWS Code Habits | https://github.com/awslabs/aws-code-habits
+curl -sL https://raw.githubusercontent.com/awslabs/aws-code-habits/main/scripts/remote/init.sh | bash
