f3b847e6cedadcc74155e8e36daa824d9562d25a
Valter Silva <valterh@amazon.com>
2022-10-27 15:12:34 +0000

    Install AWS Code Habits

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -3,9 +3,5 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-# copy Makefile to current workspace, only if doesn't exist
-if [ ! -f Makefile ]; then
-    cp .devcontainer/Makefile ./
-fi
-
-make init
+# Install AWS Code Habits | https://github.com/awslabs/aws-code-habits
+curl -sL https://raw.githubusercontent.com/awslabs/aws-code-habits/main/scripts/remote/init.sh | bash
