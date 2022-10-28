8abd7bd0370b27ce0e389a600ca15a2bd0d9d152
Valter Silva <valterh@amazon.com>
2022-10-28 13:40:11 +0800

    Install AWS Code Habits during post-start

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -3,4 +3,6 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-clear && printf "\e[0;32mAWS Terraform Dev Container: $(basename $PWD)\e[0m\n"
+clear 
+printf "\e[0;32mAWS Terraform Dev Container: $(basename $PWD)\e[0m\n"
+devcontainer-info
