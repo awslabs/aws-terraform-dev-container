a967630d9478ebd48097d1e6d3dafb44ee8e6ae3
Valter Silva <valterh@amazon.com>
2022-10-28 15:18:40 +0800

    Use main branch once merged has been done

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 set -o pipefail
 
-git clone --branch=feature/aws-code-habits --depth=1 git@github.com:awslabs/aws-terraform-dev-container.git
+git clone --branch=main --depth=1 git@github.com:awslabs/aws-terraform-dev-container.git
 mv aws-terraform-dev-container/.devcontainer .devcontainer
 rm -rf aws-terraform-dev-container
 
