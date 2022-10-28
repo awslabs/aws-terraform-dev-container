baac71088e7727f5d189cd8f3181ae3cd1354e1a
Valter Silva <valterh@amazon.com>
2022-10-28 13:24:22 +0800

    Add AWS Code Habits as git submodule

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,4 +9,5 @@ mv aws-terraform-dev-container/.devcontainer .devcontainer
 rm -rf aws-terraform-dev-container
 
 # Install AWS Code Habits | https://github.com/awslabs/aws-code-habits
-curl -sL https://raw.githubusercontent.com/awslabs/aws-code-habits/main/scripts/remote/init.sh | bash
+git submodule add --name habits -b main https://github.com/awslabs/aws-code-habits.git habits
+cp habits/scripts/Makefile Makefile
